86dc484a4c664b06d1886b77a6a90cea862b68d0
Анатолий Злотников <Zlotnikov@MacBook-Air-Anatolij.local>
2019-12-19 19:29:16 +0300

    hbs 2.0

--- middleware/db-connect.js
+++ middleware/db-connect.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/labyrinth", {
+mongoose.connect("mongodb+srv://labUser:llbbnn123@cluster0-epnwi.mongodb.net/test?retryWrites=true&w=majority", {
     useNewUrlParser: true, useUnifiedTopology: true
 });
 
