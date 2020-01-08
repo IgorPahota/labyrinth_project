71f7ea2c1b004dc9207c6d242a2967f5a05ed315
Анатолий Злотников <Zlotnikov@MacBook-Air-Anatolij.local>
2020-01-08 19:11:08 +0300

    added starting position

--- public/client.js
+++ public/client.js
@@ -29,6 +29,22 @@ document.querySelector('.keyboard').addEventListener('click', function (event) {
 });
 
 let color = 'grey';
+let positionChecker = false;
+
+function positionStart() {
+    if (!document.querySelector('[dataMouse=true]')) {
+        positionChecker = true;
+    }
+}
+
+document.querySelector(".field").addEventListener('click', function (event) {
+    if (positionChecker) {
+        let element = event.target;
+        element.setAttribute("dataMouse", true)
+        element.style.backgroundColor = 'black';
+        positionChecker = false;
+    }
+})
 
 function changeColor(newColor) {
     color = newColor;
@@ -38,9 +54,6 @@ function colorClick(element, newColor) {
     newColor = color;
     element.setAttribute("dataColor", newColor);
     element.style.backgroundColor = newColor;
-    if (newColor === 'green') {
-        element.setAttribute("dataMouse", true)
-    }
 };
 
 document.addEventListener('keydown', (e) => {
@@ -53,57 +66,69 @@ document.addEventListener('keydown', (e) => {
     switch (e.key) {
         case 'ArrowDown':
             if (mouse.attributes.dataRow.value < 11 && (document.querySelector(`div[dataRow="${Number(mouse.attributes.dataRow.value) + 1}"][dataColumn="${mouse.attributes.dataColumn.value}"]`)).style.backgroundColor !== 'darkblue') {
-            mouse.style.backgroundColor = prevColor;
-            document.querySelector('.actions').value += mouse.innerHTML + '-';
-            document.querySelector('.actions').value += count + ',';
-            moveColumn = mouse.attributes.dataColumn.value;
-            moveRow = Number(mouse.attributes.dataRow.value) + 1;
-            mouse.setAttribute('dataMouse', 'false');
+                mouse.style.backgroundColor = prevColor;
+                document.querySelector('.actions').value += mouse.innerHTML + '-';
+                document.querySelector('.actions').value += count + ',';
+                moveColumn = mouse.attributes.dataColumn.value;
+                moveRow = Number(mouse.attributes.dataRow.value) + 1;
+                mouse.setAttribute('dataMouse', 'false');
 
-            nextCell = document.querySelector(`div[dataRow="${moveRow}"][dataColumn="${moveColumn}"]`);
-            nextCell.setAttribute('dataMouse', true);
-            nextCell.style.backgroundColor = 'black';
-            break;} else {break}
+                nextCell = document.querySelector(`div[dataRow="${moveRow}"][dataColumn="${moveColumn}"]`);
+                nextCell.setAttribute('dataMouse', true);
+                nextCell.style.backgroundColor = 'black';
+                break;
+            } else {
+                break
+            }
         case 'ArrowUp':
             if (mouse.attributes.dataRow.value > 1 && (document.querySelector(`div[dataRow="${mouse.attributes.dataRow.value - 1}"][dataColumn="${mouse.attributes.dataColumn.value}"]`)).style.backgroundColor !== 'darkblue') {
-            mouse.style.backgroundColor = prevColor;
-            document.querySelector('.actions').value += mouse.innerHTML + '-';
-            document.querySelector('.actions').value += count + ',';
-            moveColumn = mouse.attributes.dataColumn.value;
-            moveRow = mouse.attributes.dataRow.value - 1;
-            mouse.setAttribute('dataMouse', 'false');
+                mouse.style.backgroundColor = prevColor;
+                document.querySelector('.actions').value += mouse.innerHTML + '-';
+                document.querySelector('.actions').value += count + ',';
+                moveColumn = mouse.attributes.dataColumn.value;
+                moveRow = mouse.attributes.dataRow.value - 1;
+                mouse.setAttribute('dataMouse', 'false');
 
-            nextCell = document.querySelector(`div[dataRow="${moveRow}"][dataColumn="${moveColumn}"]`);
-            nextCell.setAttribute('dataMouse', true);
-            nextCell.style.backgroundColor = 'black';
-            break;} else {break}
+                nextCell = document.querySelector(`div[dataRow="${moveRow}"][dataColumn="${moveColumn}"]`);
+                nextCell.setAttribute('dataMouse', true);
+                nextCell.style.backgroundColor = 'black';
+                break;
+            } else {
+                break
+            }
         case 'ArrowLeft':
             if (mouse.attributes.dataColumn.value > 1 && (document.querySelector(`div[dataRow="${mouse.attributes.dataRow.value}"][dataColumn="${mouse.attributes.dataColumn.value - 1}"]`)).style.backgroundColor !== 'darkblue') {
-            console.log(e.key)
-            mouse.style.backgroundColor = prevColor;
-            document.querySelector('.actions').value += mouse.innerHTML + '-';
-            document.querySelector('.actions').value += count + ',';
-            moveColumn = mouse.attributes.dataColumn.value - 1;
-            moveRow = mouse.attributes.dataRow.value;
-            mouse.setAttribute('dataMouse', 'false');
+                console.log(e.key)
+                mouse.style.backgroundColor = prevColor;
+                document.querySelector('.actions').value += mouse.innerHTML + '-';
+                document.querySelector('.actions').value += count + ',';
+                moveColumn = mouse.attributes.dataColumn.value - 1;
+                moveRow = mouse.attributes.dataRow.value;
+                mouse.setAttribute('dataMouse', 'false');
 
-            nextCell = document.querySelector(`div[dataRow="${moveRow}"][dataColumn="${moveColumn}"]`);
-            nextCell.setAttribute('dataMouse', true);
-            nextCell.style.backgroundColor = 'black';
-            break;} else {break}
+                nextCell = document.querySelector(`div[dataRow="${moveRow}"][dataColumn="${moveColumn}"]`);
+                nextCell.setAttribute('dataMouse', true);
+                nextCell.style.backgroundColor = 'black';
+                break;
+            } else {
+                break
+            }
         case 'ArrowRight':
             if (mouse.attributes.dataColumn.value < 11 && (document.querySelector(`div[dataRow="${mouse.attributes.dataRow.value}"][dataColumn="${Number(mouse.attributes.dataColumn.value) + 1}"]`)).style.backgroundColor !== 'darkblue') {
-            mouse.style.backgroundColor = prevColor;
-            document.querySelector('.actions').value += mouse.innerHTML + '-';
-            document.querySelector('.actions').value += count + ',';
-            moveColumn = Number(mouse.attributes.dataColumn.value) + 1;
-            moveRow = mouse.attributes.dataRow.value;
-            mouse.setAttribute('dataMouse', 'false');
+                mouse.style.backgroundColor = prevColor;
+                document.querySelector('.actions').value += mouse.innerHTML + '-';
+                document.querySelector('.actions').value += count + ',';
+                moveColumn = Number(mouse.attributes.dataColumn.value) + 1;
+                moveRow = mouse.attributes.dataRow.value;
+                mouse.setAttribute('dataMouse', 'false');
 
-            nextCell = document.querySelector(`div[dataRow="${moveRow}"][dataColumn="${moveColumn}"]`);
-            nextCell.setAttribute('dataMouse', true);
-            nextCell.style.backgroundColor = 'black';
-            break;} else {break}
+                nextCell = document.querySelector(`div[dataRow="${moveRow}"][dataColumn="${moveColumn}"]`);
+                nextCell.setAttribute('dataMouse', true);
+                nextCell.style.backgroundColor = 'black';
+                break;
+            } else {
+                break
+            }
     }
 });
 
