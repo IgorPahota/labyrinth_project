9353d996e095480465941700066255b5e1426e3d
Анатолий Злотников <Zlotnikov@MacBook-Air-Anatolij.local>
2020-01-08 14:57:55 +0300

    Virtual keyboard click-priority fixed

--- public/client.js
+++ public/client.js
@@ -2,11 +2,13 @@ let clickCount = 0;
 
 document.querySelector('.keyboard').addEventListener('click', function (event) {
     function singleClick() {
+        console.log(event.target.parentElement.firstChild)
         document.querySelector('.actions').value += event.target.parentElement.firstChild.innerText + '-';
         document.querySelector('.actions').value += count + ','
     }
 
     function doubleClick() {
+        console.log(event.target.parentElement.lastChild)
         document.querySelector('.actions').value += event.target.parentElement.lastChild.innerText + '-';
         document.querySelector('.actions').value += count + ','
     }
