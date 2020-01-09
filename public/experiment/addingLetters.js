1c12583ce17a17aeb68ca0db17ec8e282074f35a
Igor Pahota <eto.pochtovaya@gmail.com>
2020-01-09 20:25:14 +0300

    adding new field

---
+++ public/experiment/addingLetters.js
@@ -0,0 +1,26 @@
+document.querySelector('.field').addEventListener('click', (event)=>{
+   event.stopPropagation();
+   console.log(event.target);
+   // console.log(event.firstChild);
+    function removed () {
+        console.log('removed')
+    }
+
+    let clickedDiv = event.target;
+
+    const keyHandler =  (event) => {
+        event.preventDefault();
+        console.log('pressed')
+        // event.stopPropagation();
+        // let pressedButton = event.key;
+        // console.log(clickedDiv.innerHTML);
+        clickedDiv.innerHTML = event.key;
+        document.removeEventListener('keydown', keyHandler);
+        console.log('removed')
+
+    }
+
+    document.addEventListener('keydown', keyHandler);
+
+});
+
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const Lab = require('../models/labyrinth');
 const router = express.Router();
 
+
+
+// Seeder
 router.get('/', (req, res) => {
     let testField = new Lab ({
         name: 'testing'
@@ -10,6 +13,42 @@ router.get('/', (req, res) => {
     testField.save();
     res.send('seed done')
 });
+
+router.post('/', async (req, res) => {
+    // console.log(req.body.savedField)
+    let newField = new Lab ({
+        name: req.body.savedField.name,
+        dateOfCreation: req.body.savedField.dateOfCreation,
+        field: req.body.savedField.field
+    })
+   await newField.save()
+    let currentField = await Lab.findOne({dateOfCreation: req.body.savedField.dateOfCreation})
+    // console.log(currentField);
+    res.json(currentField._id)
+
+
+})
+
+router.get('/experiment/:id', async (req, res) =>{
+    // console.log(req.params.id)
+    let id = req.params.id
+    let test = await Lab.findOne({_id:id})
+    // console.log(test)
+    let a = test.field[0]
+        b = test.field[1],
+        c = test.field[2],
+        d = test.field[3],
+        e = test.field[4],
+        f = test.field[5],
+        g = test.field[6],
+        h = test.field[7],
+        i = test.field[8],
+        j = test.field[9],
+        k = test.field[10]
+    console.log(a)
+     // await res.send(a, b, c, d, e, f, g, h, i, j, k)
+    await res.render('current', {a, b, c, d, e, f, g, h, i, j, k})
+})
 
 router.get('/field', async (req, res) => {
     let test = await Lab.find({});
@@ -24,7 +63,7 @@ router.get('/field', async (req, res) => {
         i = test[0].field[8],
         j = test[0].field[9],
         k = test[0].field[10]
-    await res.render('field', {a, b, c, d, e, f, g, h, i, j, k})
+     await res.render('field', {a, b, c, d, e, f, g, h, i, j, k})
 });
 
 module.exports = router;
